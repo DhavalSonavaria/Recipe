0670fb5aec492ae6573854acf0fb8cbeedcc2174
unknown <dhsonava@syr.edu>
2022-12-26 00:07:23 -0800

    added files

---
+++ server/models/Recipe.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: 'This field is required.'
+  },
+  description: {
+    type: String,
+    required: 'This field is required.'
+  },
+  author: {
+    type: String,
+    required: 'This field is required.'
+  },
+  ingredients: {
+    type: Array,
+    required: 'This field is required.'
+  },
+  category: {
+    type: String,
+    enum: ['Dessert', 'Dinner', 'Holiday', 'Baked-goods', 'Cocktails'],
+    required: 'This field is required.'
+  },
+  image: {
+    type: String,
+    required: 'This field is required.'
+  },
+});
+
+recipeSchema.index({ name: 'text', description: 'text' });
+// WildCard Indexing
+//recipeSchema.index({ "$**" : 'text' });
+
+module.exports = mongoose.model('Recipe', recipeSchema);
